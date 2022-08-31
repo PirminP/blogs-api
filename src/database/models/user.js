9b7268a6d1b13518d3614eda826480006bff84e6
PirminP <pirmin.pernsteiner@gmail.com>
2022-08-31 20:38:24 -0300

    feat: Creating User model with correct properties

---
+++ src/database/models/user.js
@@ -0,0 +1,17 @@
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define('User', {
+    id: {
+      autoIncrement: true,
+      primaryKey: true,
+      type: DataTypes.INTEGER
+    },
+    displayName: DataTypes.STRING,
+    email: DataTypes.STRING,
+    password: DataTypes.STRING,
+    image: DataTypes.STRING
+  },
+  { timestamps: false },
+  );
+
+  return User;
+};
