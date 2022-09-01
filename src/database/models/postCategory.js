1dc8d87865df768f7c8254a78431546f84277899
PirminP <pirmin.pernsteiner@gmail.com>
2022-08-31 21:33:59 -0300

    feat: Creating model PostCategory with correct properties & associatens

---
+++ src/database/models/postCategory.js
@@ -0,0 +1,26 @@
+module.exports = (sequelize, DataTypes) => {
+    const PostCategory = sequelize.define('PostCategory', {
+      postId: DataTypes.INTEGER,
+      categoryId: DataTypes.INTEGER
+    },
+    { timestamps: false },
+    );
+  
+    PostCategory.associate = (models) => {
+      models.Category.belongsToMany(models.BlogPost, {
+        as: 'blogpost',
+        through: PostCategory,
+        foreignKey: 'postId',
+        otherKey: 'categoryId'
+      });
+
+      models.BlogPost.belongsToMany(models.Category, {
+        as: 'category',
+        through: PostCategory,
+        foreignKey: 'categoryId',
+        otherKey: 'postId'
+        });
+    };
+  
+    return PostCategory;
+  };
